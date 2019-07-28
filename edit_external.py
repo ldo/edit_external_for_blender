d45a9573f82b4d80bf0fa80754d934012341abcb
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2019-07-28 03:11:55 +0000

    update for Blender 2.8

--- edit_external.py
+++ edit_external.py
@@ -15,8 +15,8 @@ bl_info = \
     {
         "name" : "Edit in Emacs",
         "author" : "Lawrence D'Oliveiro <ldo@geek-central.gen.nz>",
-        "blender" : (2, 6, 3),
-        "version" : (1, 0, 1),
+        "version" : (1, 0, 2),
+        "blender" : (2, 80, 0),
         "location" : "Text Editor > Text > Edit in Emacs",
         "description" : "Edit the current text externally in Emacs (ALT+E)",
         "category" : "Text Editor",
@@ -31,16 +31,16 @@ class EditExternal(bpy.types.Operator) :
         if context.edit_text != None :
             the_text = context.edit_text.as_string()
             tempfile_fd, tempfile_name = tempfile.mkstemp(text = True)
-            the_file = os.fdopen(tempfile_fd, "r+")
+            the_file = os.fdopen(tempfile_fd, "rb+")
             the_file.seek(0, os.SEEK_SET)
             the_file.truncate()
-            the_file.write(the_text)
+            the_file.write(the_text.encode())
             the_file.flush()
             status = os.system("emacsclient " + tempfile_name)
             if status == 0 :
                 the_file.seek(0, os.SEEK_SET)
                 context.edit_text.clear()
-                context.edit_text.write(the_file.read())
+                context.edit_text.write(the_file.read().decode())
             #end if
             the_file.close()
             #os.close(tempfile_fd) # done by closing file object
@@ -56,27 +56,21 @@ class EditExternal(bpy.types.Operator) :
 #end EditExternal
 
 def add_to_menu(self, context) :
-    self.layout.operator(EditExternal.bl_idname, icon = "EDIT")
+    self.layout.operator(EditExternal.bl_idname, icon = "TEXT")
 #end add_to_menu
 
 def register() :
     bpy.utils.register_class(EditExternal)
-    the_keymap = bpy.context.window_manager.keyconfigs["Blender"].keymaps["Text"]
+    the_keymap = bpy.context.window_manager.keyconfigs["blender"].keymaps["Text"]
     args = [EditExternal.bl_idname]
     kwargs = {"type" : "E", "value" : "PRESS", "alt" : True, "head" : True}
-    try :
-        the_keymap.keymap_items.new(*args, **kwargs)
-    except TypeError :
-        # assume pre-SVN-r50276, no “head” arg
-        del kwargs["head"]
-        the_keymap.keymap_items.new(*args, **kwargs)
-    #end try
+    the_keymap.keymap_items.new(*args, **kwargs)
     bpy.types.TEXT_MT_text.append(add_to_menu)
 #end register
 
 def unregister() :
     bpy.types.TEXT_MT_text.remove(add_to_menu)
-    the_keymap = bpy.context.window_manager.keyconfigs["Blender"].keymaps["Text"]
+    the_keymap = bpy.context.window_manager.keyconfigs["blender"].keymaps["Text"]
     for the_key in the_keymap.keymap_items :
         if the_key.idname == EditExternal.bl_idname :
             the_keymap.keymap_items.remove(the_key)
